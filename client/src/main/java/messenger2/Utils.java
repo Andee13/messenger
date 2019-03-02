dbd1948680fc0905bf5768a334b44cc8d20a2741
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-03 00:40:04 +0200

    hi

---
+++ client/src/main/java/messenger2/Utils.java
@@ -0,0 +1,11 @@
+package messenger2.Controllers;
+
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Utils {
+    public static Socket socket;
+    public static InputStreamReader reader;
+    public static OutputStreamWriter writer;
+}
