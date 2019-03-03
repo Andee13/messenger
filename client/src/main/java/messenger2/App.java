17b009f6c93b3062e01824d3eb4e2b24a241729f
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-04 01:56:44 +0200

    Registration and login logic is finished

--- client/src/main/java/messenger2/App.java
+++ client/src/main/java/messenger2/App.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import static messenger2.Utils.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 
 public class App extends Application {
@@ -19,6 +22,10 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
+
+        socket = new Socket("localhost", 5940);
+        reader = new DataInputStream(socket.getInputStream());
+        writer = new DataOutputStream(socket.getOutputStream());
 
         Parent root = FXMLLoader.load(getClass().getResource("/messenger2/views/Login.fxml"));
         primaryStage.setTitle("Hello World");
